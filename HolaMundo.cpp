536382ede4e567b759c015b9c1e396f1ffd70c70
RaulRamiraRivera <79485110+RaulRamiraRivera@users.noreply.github.com>
2021-02-24 00:27:45 -0600

    Add files via upload

---
+++ HolaMundo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout <<"hola Raul Ramirez Rivera" << endl;
+}
+/*
+///////////////////////////////////////////
+#include <iostream>
+int main(){
+std:: cout <<"hola "<< std::endl;
+std:: cout <<"Raúl"<< std:: endl;
+std:: cout <<"Ramirez"<< std:: endl;
+std:: cout <<"Rivera" << std:: endl;
+}
+//////////////////////////////////////////
+#include <iostream>
+using std:: cout;
+using std:: endl;
+int main(){
+    cout <<"hola Raúl Ramirez Rivera "<< endl ;
+}
+///////////////////////////////////////////
+#include<stdio.h>
+int main(){
+	printf("Hola Raul Ramirez Rivera\n");
+}
+*/
